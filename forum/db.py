2534403820ad1ee4b54a450722d813cc83be800e
Yuang Fan <yuangf2@illinois.edu>
2022-10-31 04:13:47 -0500

    Delete db.py

--- forum/db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-
-# For using sqlalchemy ORM
-db = SQLAlchemy()
-
-# "mysql+pymysql://username:password@host:port/database"
-engine = create_engine(
-    "mysql+pymysql://root:qwerty12@coursepartnermatcher.cpzyc87uemgs.us-east-2.rds.amazonaws.com/CoursePartnerMatcher")
